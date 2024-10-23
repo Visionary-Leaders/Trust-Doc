2785a090a89d7dc0ec7a3d368378a537dea7918e
professorDeveloper <azamovhud007@outlook.com>
2024-10-23 09:18:18 +0500

    Authorization Created )

---
+++ app/src/main/java/com/trustio/importantdocuments/ui/screens/auth/LoginPage.kt
@@ -0,0 +1,18 @@
+package com.trustio.importantdocuments.ui.screens.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.trustio.importantdocuments.R
+import com.trustio.importantdocuments.databinding.LoginPageBinding
+import com.trustio.importantdocuments.utils.BaseFragment
+
+class LoginPage :BaseFragment<LoginPageBinding>(LoginPageBinding::inflate){
+    override fun onViewCreate(savedInstanceState: Bundle?) {
+        binding.apply {
+
+        }
+    }
+}
