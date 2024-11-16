f8178920d018a3cf034331dc87694a6bad559d99
professorDeveloper <azamovhud007@outlook.com>
2024-11-16 20:03:25 +0500

    Hello NDK

---
+++ app/src/main/cpp/importantdocuments.cpp
@@ -0,0 +1,24 @@
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("importantdocuments");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("importantdocuments")
+//      }
+//    }
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
